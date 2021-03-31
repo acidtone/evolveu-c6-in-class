538c636d5980e961bd5ba36e65905abfd455a09e
Tony Grimes <acidtone@tonygrimes.com>
2021-03-31 14:21:29 -0600

    mid afternoon code

---
+++ w03d03-hello-express/hello-express/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+const chest = {
+  treasure: [
+    'coins',
+    'gem',
+    'enchanted sword'
+  ]
+}
+
+app.get('/', (request, response) => {
+  response.send("Hello World!");
+})
+
+
+app.get('/forest', function(request, response) {
+  response.send("You are in a deep, dark wood...")
+})
+
+app.get('/vault', function(request, response){
+  response.send({
+    trophies: [
+      'rusty dagger',
+      'cursed peanut'
+    ]
+  })
+})
+
+app.get('/chest', function(request, response) {
+  response.send(chest)
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+})
